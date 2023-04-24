e62f452e886608612c17ce1444fd1e2ae4222536
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-04-24 20:11:27 +0400

    done with telegram-bot

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from decouple import config
-import requests
 import json
+
+import requests
+import telegram
+from decouple import config
+
+
+def send_tg_message(token, lesson_title, result, lesson_url):
+    bot = telegram.Bot(token)
+    chat_id = config('TG_CHAT_ID')
+    message_to_customer = f"У вас проверили работу '[{lesson_title}]({lesson_url})'\n{result}"
+    bot.send_message(chat_id=chat_id, text=message_to_customer, parse_mode="Markdown", disable_web_page_preview=True)
 
 
 def get_reviews(token, timestamp):
@@ -13,22 +22,32 @@ def get_reviews(token, timestamp):
     }
     response = requests.get(api_endpoint, headers=headers, timeout=90, params=payload)
     response.raise_for_status()
-    reviews = response.json()
-    return reviews
+    response.json()
+    return response.json()
 
 
 if __name__ == "__main__":
     devman_token = config('DEVMAN_TOKEN')
+    tg_bot_token = config('TELEGRAM_BOT_TOKEN')
     timestamp = None
+    result = None
+    lesson_title = None
+    lesson_url = None
     while True:
         try:
             reviews = get_reviews(devman_token, timestamp)
             json_formatted_str = json.dumps(reviews, indent=2)
-            print(json_formatted_str)
             if reviews["status"] == "found":
                 timestamp = reviews["last_attempt_timestamp"]
             else:
                 timestamp = reviews["timestamp_to_request"]
+            if reviews["new_attempts"][0]["is_negative"]:
+                result = "К сожалению, в работе нашлись ошибки"
+            else:
+                result = "Преподавателю все понравилось, можно приступать к следующему уроку"
+            lesson_title = reviews["new_attempts"][0]["lesson_title"]
+            lesson_url = reviews["new_attempts"][0]["lesson_url"]
+            send_tg_message(tg_bot_token, lesson_title, result, lesson_url)
         except requests.exceptions.HTTPError:
             print('Неверная ссылка')
         except requests.exceptions.ReadTimeout:
